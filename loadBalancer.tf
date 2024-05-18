a77f0babc50ad38e51b6ab067f74604cb714f1ad
Rahma Hassan <rahmaashraf053@gmail.com>
2024-05-19 01:03:43 +0300

    modify load balancer

--- loadBalancer.tf
+++ loadBalancer.tf
@@ -38,7 +38,7 @@ resource "aws_lb_target_group" "alb_target_group" {
   name        = "alb-target-group"
   port        = 3000
   protocol    = "HTTP"
-  vpc_id = module.network-lab2.network.vpc_id
+  vpc_id = module.network-lab2.vpc_id
 
 }
 
