0d20e232d6236765cd8a13df371b5a53120278a4
Rahma Hassan <rahmaashraf053@gmail.com>
2024-05-18 20:39:37 +0300

    adding egress security group

--- instances.tf
+++ instances.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "ssh_and_3000" {
     protocol    = "tcp"
     cidr_blocks = [module.network-lab2.vpc_cidr]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"   
+    cidr_blocks     = ["0.0.0.0/0"] 
+  }
 }
 
 # ===== Key Pair Generation ===== 
