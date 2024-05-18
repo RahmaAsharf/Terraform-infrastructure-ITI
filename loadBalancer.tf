3b29a4e8ca9eb203173a945c537af5d5aaddbbb8
Rahma Hassan <rahmaashraf053@gmail.com>
2024-05-19 00:59:39 +0300

    adding security group for load balancer

--- loadBalancer.tf
+++ loadBalancer.tf
@@ -1,10 +1,32 @@
+# Define a security group for alb
+resource "aws_security_group" "alb_sg" {
+  vpc_id        = module.network.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_alb"
+  }
+}
 # Create an ALB
 resource "aws_lb" "node_alb" {
   name               = "node-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [module.network-lab2.subnets[1].id,module.network-lab2.subnets[2].id ]
+  subnets            = [module.network-lab2.subnets[1].id, module.network-lab2.subnets[2].id ]
 
   tags = {
     Name = "Nodejs Alb"
@@ -16,7 +38,7 @@ resource "aws_lb_target_group" "alb_target_group" {
   name        = "alb-target-group"
   port        = 3000
   protocol    = "HTTP"
-  vpc_id = module.network.vpc_id
+  vpc_id = module.network-lab2.network.vpc_id
 
 }
 
